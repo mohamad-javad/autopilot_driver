d82b5464afc6a481edef2586f404acdaa1ce1fe0
Mohammad Javad <mohamad-javad@users.noreply.github.com>
2024-07-08 03:08:35 +0330

    Add Frequency setting to 50 Hz for attitude and raw_imu. - add error handling for frequency setting. - add dynamic port connection for serial.

--- autopilot_driver/apu_node.py
+++ autopilot_driver/apu_node.py
@@ -1,4 +1,7 @@
 import time
+import os
+
+import serial.serialutil
 
 import rclpy
 from rclpy.node import Node
@@ -8,8 +11,8 @@ from geometry_msgs.msg import Quaternion, Twist
 
 from mavros_msgs.msg import GimbalDeviceAttitudeStatus, GPSRAW, GPSINPUT, VfrHud
 from mavros_msgs.srv import CommandLong
-from pymavlink import mavutil as mavu
-from pymavlink.dialects.v20.smrc import MAVLink_autoware_mpu_message as MPU_Message
+from pymavlink import mavutil as mavu, mavparm
+from pymavlink.dialects.v20.smrc import MAVLink_ipc_data_message as MPU_Message
 from pymavlink.dialects.v20.smrc import *
 
 from autopilot_msgs.msg import Mpu
@@ -19,6 +22,9 @@ from autopilot_msgs.srv import SendGPS, SendMPUMsg
 class Autopilot(Node):
     def __init__(self):
         super().__init__('Autopilot_Base')
+        self.attitude_msg = None
+        self.imu_msg = None
+        self.vfrHdu_msg = None
         self.imu_publisher_ = self.create_publisher(Imu, '/autopilot/imu', 10)
         self.attitude_publisher_ = self.create_publisher(
             GimbalDeviceAttitudeStatus, '/autopilot/attitude', 10
@@ -29,8 +35,12 @@ class Autopilot(Node):
         self.mpu_service_ = self.create_service(
             SendMPUMsg, '/autopilot/mpu_srv', self.mpu_msg_responder)
 
+        self.mav_parm = mavparm.MAVParmDict()
         self.initialize_connection()
-        self.reciever_timer = self.create_timer(0.01, self.reciever_callback)
+        self.receiver_timer = self.create_timer(0.001, self.receiver_callback)
+        self.vfrHud_pub_timer = self.create_timer(1/50, self.vfrHud_pub_callback)
+        self.imu_pub_timer = self.create_timer(1/50, self.imu_pub_callback)
+        self.att_pub_timer = self.create_timer(1/50, self.att_pub_callback)
         self.att_msg = None
         self.gps_msg: GPSINPUT = None
         self.mpu_msg: Mpu = None
@@ -40,32 +50,74 @@ class Autopilot(Node):
     def initialize_connection(self):
         while True:
             try:
-                self.serial_connection = mavu.mavlink_connection('/dev/ttyACM0', baud=115200)
+                serial_path = '/dev/ttyACM'
+                for i in range(10):
+                    if os.path.exists(serial_path + str(i)):
+                        serial_path += str(i)
+                        break
+                self._logger.info(f"Connecting to Autopilot in port {serial_path}")
+                self.serial_connection = mavu.mavlink_connection(serial_path, baud=921600)
                 self.serial_connection.wait_heartbeat()
                 break
             except Exception as e:
                 self._logger.error('Error in connection: {}'.format(e))
                 time.sleep(1)
-        self._logger.info('Connection established.')
+        try:
+            self._logger.info("Setting Attitude Frequency to 50")
+            assert self.mav_parm.mavset(
+               self.serial_connection, "sr0_extra1", 50, retries=5
+            ), "Cant Set Attitude Frequency"
+            self._logger.info("==> Done!")
+        except Exception as e:
+            self._logger.error('Error in setting frequency: {}'.format(e))
+        try:
+            self._logger.info("Setting Raw_IMU Frequency to 50")
+            assert self.mav_parm.mavset(
+                   self.serial_connection, "sr0_raw_sens", 50, retries=5
+            ), "Cant Set Attitude Frequency"
+            self._logger.info("==> Done!")
+        except Exception as e:
+            self._logger.error('Error in setting frequency: {}'.format(e))
+            # try:
+            #     self._logger.info("Setting VFR_HUD Frequency to 50")
+            #     att_freq = False
+            #     while not att_freq:
+            #         att_freq = self.mav_parm.mavset(self.serial_connection, "sr0_", 50)
+            # except Exception as e:
+            #     self._logger.error('Error in setting frequency: {}'.format(e))
 
-    def reciever_callback(self):
-        msg = self.serial_connection.recv_msg()
-        if msg is not None and msg.get_type() != 'BAD_DATA':
-            if msg.get_type() == 'VFR_HUD':
-                self.vfr_hud_callback(msg)
-            elif msg.get_type() == 'ATTITUDE':
-                self.attitude_callback(msg)
-                self.att_msg = msg
-            elif msg.get_type() == 'RAW_IMU' and self.att_msg is not None:
-                self.imu_callback(self.att_msg, msg)
-            # else:
-            # self._logger.info('Message type: {}'.format(msg.get_type()))
+        self._logger.info(f'Connection is stablished to port: <{serial_path}>')
+
+    def receiver_callback(self):
+        try:
+            msg = self.serial_connection.recv_msg()
+            if msg is not None and msg.get_type() != 'BAD_DATA':
+                if msg.get_type() == 'VFR_HUD':
+                    self.vfr_hud_callback(msg)
+                elif msg.get_type() == 'ATTITUDE':
+                    self.attitude_callback(msg)
+                    self.att_msg = msg
+                elif msg.get_type() == 'RAW_IMU':
+                    self.imu_callback(msg)
+        except serial.serialutil.SerialException as e:
+            self._logger.error("Error in serial connection: ".format(e))
+            self.initialize_connection()
+
+        except Exception as e:
+            self._logger.error("Error in reading message: ".format(e))
+
+        try:
             if self.is_gps_msg:
                 self.serial_connection.mav.send(self.gps_msg)
                 self.is_gps_msg = False
             if self.is_mpu_msg:
                 self.serial_connection.mav.send(self.mpu_msg)
                 self.is_mpu_msg = False
+        except serial.serialutil.SerialException as e:
+            self._logger.error(f"Error in sending message: {e}")
+        except Exception as e:
+            self._logger.error("Error in sending message: ".format(e))
+            self.initialize_connection()
 
     def vfr_hud_callback(self, msg):
         _msg = VfrHud()
@@ -76,7 +128,12 @@ class Autopilot(Node):
         _msg.throttle = float(msg.throttle)
         _msg.climb = msg.climb
 
-        self.vfrHud_publisher_.publish(_msg)
+        self.vfrHdu_msg = _msg
+
+    def vfrHud_pub_callback(self):
+        if not self.vfrHdu_msg is None:
+            self.vfrHud_publisher_.publish(self.vfrHdu_msg)
+            self.vfrHdu_msg = None
 
     def attitude_callback(self, msg):
         _msg = GimbalDeviceAttitudeStatus()
@@ -88,25 +145,36 @@ class Autopilot(Node):
         _msg.angular_velocity_x = msg.rollspeed
         _msg.angular_velocity_y = msg.pitchspeed
         _msg.angular_velocity_z = msg.yawspeed
+        self.attitude_msg = _msg
 
-        self.attitude_publisher_.publish(_msg)
+    def att_pub_callback(self):
+        if self.attitude_msg is not None:
+            self.attitude_publisher_.publish(self.attitude_msg)
+            self.attitude_msg = None
 
-    def imu_callback(self, att_msg, imu_msg):
+    def imu_callback(self, imu_msg):
         _msg = Imu()
         _msg.header.stamp = self.get_clock().now().to_msg()
-        _msg.orientation.x = att_msg.roll
-        _msg.orientation.y = att_msg.pitch
-        _msg.orientation.z = att_msg.yaw
+
         _msg.orientation.w = 0.0
-        _msg.angular_velocity.x = float(att_msg.rollspeed)
-        _msg.angular_velocity.y = float(att_msg.pitchspeed)
-        _msg.angular_velocity.z = float(att_msg.yawspeed)
+
         _msg.linear_acceleration.x = float(imu_msg.xacc)
         _msg.linear_acceleration.y = float(imu_msg.yacc)
         _msg.linear_acceleration.z = float(imu_msg.zacc)
 
-        self.imu_publisher_.publish(_msg)
+        self.imu_msg = _msg
 
+    def imu_pub_callback(self):
+        if self.imu_msg is not None and self.att_msg is not None:
+            self.imu_msg.orientation.x = self.att_msg.roll
+            self.imu_msg.orientation.y = self.att_msg.pitch
+            self.imu_msg.orientation.z = self.att_msg.yaw
+            self.imu_msg.angular_velocity.x = float(self.att_msg.rollspeed)
+            self.imu_msg.angular_velocity.y = float(self.att_msg.pitchspeed)
+            self.imu_msg.angular_velocity.z = float(self.att_msg.yawspeed)
+            self.imu_publisher_.publish(self.imu_msg)
+            self.imu_msg = None
+        return
 
     def gps_msg_responder(self, req: SendGPS.Request, res: SendGPS.Response):
         try:
@@ -131,7 +199,6 @@ class Autopilot(Node):
                 vert_accuracy=int(g_msg.vert_accuracy),
                 satellites_visible=int(g_msg.satellites_visible)
             )
-
         except Exception as e:
             self._logger.error('Error in creating GPS message: {}'.format(e))
             res.success = False
@@ -147,26 +214,11 @@ class Autopilot(Node):
             self.mpu_msg = MPU_Message(
                 preamble=m_msg.preamble,
                 mode=m_msg.mode,
-                sta_ref=m_msg.sta_ref,
-                str_ref=m_msg.str_ref,
-                yaw_ref=m_msg.yaw_ref,
-                yaw_rate_ref=m_msg.yaw_rate_ref,
-                swa=m_msg.swa,
-                swar=m_msg.swar,
-                reserved0=m_msg.reserved0,
-                reserved1=m_msg.reserved1,
-                reserved2=m_msg.reserved2,
-                reserved3=m_msg.reserved3,
-                speed_ref=m_msg.speed_ref,
-                acc_ref=m_msg.acc_ref,
-                jerk_ref=m_msg.jerk_ref,
-                gpa_ref=m_msg.gpa_ref,
-                ebrake_ref=m_msg.ebrake_ref,
-                hbrake_ref=m_msg.hbrake_ref,
-                reserved4=m_msg.reserved4,
-                reserved5=m_msg.reserved5,
-                reserved6=m_msg.reserved6,
-                gear=m_msg.gear,
+                references=[m_msg.sta_ref, m_msg.str_ref, m_msg.yaw_ref, m_msg.yaw_rate_ref, m_msg.swa,
+                            m_msg.swar, m_msg.reserved0, m_msg.reserved1, m_msg.reserved2, m_msg.reserved3,
+                            m_msg.speed_ref, m_msg.acc_ref, m_msg.jerk_ref, m_msg.gpa_ref, m_msg.ebrake_ref,
+                            m_msg.hbrake_ref, m_msg.reserved4,
+                            m_msg.reserved5, m_msg.reserved6, m_msg.gear],
                 mode_fb=m_msg.mode_fb,
                 switch_fb=m_msg.switch_fb,
                 steer_fb=m_msg.steer_fb,
