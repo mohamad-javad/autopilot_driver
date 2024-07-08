d65b8fc425c52e236bd629e5297c177e8f64b17f
Mohammad Javad <mohamad-javad@users.noreply.github.com>
2024-07-09 01:12:44 +0330

    Add APU-Sensory excecutable .py file

---
+++ autopilot_driver/apu_sensory.py
@@ -0,0 +1,68 @@
+import time
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import NavSatFix
+from std_msgs.msg import String, Float32, Int8
+from geometry_msgs.msg import Quaternion, Twist
+from autoware_sensing_msgs.msg import GnssInsOrientationStamped
+from tf_transformations import quaternion_from_euler
+from numpy import float64
+
+from mavros_msgs.msg import GimbalDeviceAttitudeStatus
+
+
+class APUSensory(Node):
+    def __init__(self, node_name):
+        super().__init__(node_name)
+        self.heading_rate = None
+        self.attitude_msg = None
+
+        self.attitude_publisher_ = self.create_subscription(
+            GimbalDeviceAttitudeStatus, '/autopilot/attitude', self.attitude_subscribe_callback, 10
+        )
+        self.orientation_publisher_ = self.create_publisher(GnssInsOrientationStamped, '/autoware_orientation', 10)
+        self.heading_rate_publisher_ = self.create_publisher(String, '/heading_rate', 10)
+
+        self.heading_rate_timer = self.create_timer(1/50, self.heading_rate_callback)
+        self.orientation_timer = self.create_timer(1/50, self.autoware_orientation_callback)
+
+    def attitude_subscribe_callback(self, msg: GimbalDeviceAttitudeStatus):
+        self.attitude_msg = msg
+        self.heading_rate = msg.q.z
+
+    def heading_rate_callback(self):
+        if self.heading_rate is None:
+            return
+        msg = String()
+        msg.data = str(self.heading_rate)
+        self.heading_rate_publisher_.publish(msg)
+        self.heading_rate = None
+
+    def autoware_orientation_callback(self):
+        if self.attitude_msg is None:
+            return
+
+        _msg = GnssInsOrientationStamped()
+        _msg.header.stamp = self.get_clock().now().to_msg()
+        _msg.header.frame_id = 'base_link'
+        q = quaternion_from_euler(self.attitude_msg.q.x, self.attitude_msg.q.y, self.attitude_msg.q.z)
+        _msg.orientation.orientation.x = float64(q[0])
+        _msg.orientation.orientation.y = float64(q[1])
+        _msg.orientation.orientation.z = float64(q[2])
+        _msg.orientation.orientation.w = float64(q[3])
+
+        self.orientation_publisher_.publish(_msg)
+        self.attitude_msg = None
+
+
+def main():
+    rclpy.init()
+    apu = APUSensory("Autopilot_Sensory")
+    rclpy.spin(apu)
+    apu.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
